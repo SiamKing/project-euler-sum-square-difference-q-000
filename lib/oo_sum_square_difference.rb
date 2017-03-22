7393e8241dcd31a4730e94948cb0a377c3a23e82
SiamKing <thesiameseelectric@gmail.com>
2017-03-21 20:02:20 -0700

    dont be a square

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,12 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(num)
+    @num = num
+  end
+
+  def difference
+    arr = (1..@num).to_a
+    (arr.reduce(&:+) ** 2) - (arr.map {|num| num * num}.reduce(&:+))
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,4 @@
-# Implement your procedural solution here!
+def sum_square_difference(num)
+  arr = (1..num).to_a
+  (arr.reduce(&:+) ** 2) - (arr.map {|num| num * num}.reduce(&:+))
+end
